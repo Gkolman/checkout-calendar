8930c322b621be2f4f3c76de7bf3a52f9760ffb7
galexy n <alexneeeeewin@icloud.com>
2021-03-06 14:03:39 -0800

    [feat] added conditional to not continue creating new databases on save

--- server/models/Checkout.js
+++ server/models/Checkout.js
@@ -35,47 +35,49 @@ const checkoutDBSchema = mongoose.Schema({
 
 const CheckoutDB = mongoose.model('CheckoutDB', checkoutDBSchema);
 
-CheckoutDB.remove({}, () => console.log('cleared all rows in checkout db'));
+if (!CheckoutDB) {
 
-const getRandomInclusiveIntervals = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+  const getRandomInclusiveIntervals = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
 
-const daysNoticeOptions = [0, 1, 2, 3, 7];
-const monthsInAdvanceOptions = [0, 3, 6, 9, 12];
-const cleaningFeeOptions = [0.15, 0.3];
-const serviceFeeOptions = [0.1, 0.2];
+  const daysNoticeOptions = [0, 1, 2, 3, 7];
+  const monthsInAdvanceOptions = [0, 3, 6, 9, 12];
+  const cleaningFeeOptions = [0.15, 0.3];
+  const serviceFeeOptions = [0.1, 0.2];
 
-for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 100; i++) {
 
-  let daysNoticeIndex = getRandomInclusiveIntervals(0, 4);
-  let monthsInAdvanceIndex = getRandomInclusiveIntervals(0, 4);
+    let daysNoticeIndex = getRandomInclusiveIntervals(0, 4);
+    let monthsInAdvanceIndex = getRandomInclusiveIntervals(0, 4);
 
-  let priceForDate = Number.parseFloat(Math.random() * (300 - 75) + 75);
-  priceForDate = priceForDate.toString().split('.');
-  priceForDate[1] = (priceForDate[1].slice(0, 2));
-  priceForDate = Number.parseFloat(priceForDate.join('.'));
+    let priceForDate = Number.parseFloat(Math.random() * (300 - 75) + 75);
+    priceForDate = priceForDate.toString().split('.');
+    priceForDate[1] = (priceForDate[1].slice(0, 2));
+    priceForDate = Number.parseFloat(priceForDate.join('.'));
 
-  let cleaningFee = Number.parseFloat(Math.random() * (cleaningFeeOptions[1] - cleaningFeeOptions[0]) + cleaningFeeOptions[0]).toPrecision(4);
-  let serviceFee = Number.parseFloat(Math.random() * (serviceFeeOptions[1] - serviceFeeOptions[0]) + serviceFeeOptions[0]).toPrecision(4);
-  
+    let cleaningFee = Number.parseFloat(Math.random() * (cleaningFeeOptions[1] - cleaningFeeOptions[0]) + cleaningFeeOptions[0]).toPrecision(4);
+    let serviceFee = Number.parseFloat(Math.random() * (serviceFeeOptions[1] - serviceFeeOptions[0]) + serviceFeeOptions[0]).toPrecision(4);
 
-  let newListing = new CheckoutDB({
-    priceForDate,
-    cleaningFee,
-    serviceFee,
-    'listingId': Number(i + 1),
-    'monthsInAdvance': monthsInAdvanceOptions[monthsInAdvanceIndex],
-    'daysNotice': daysNoticeOptions[daysNoticeIndex],
-  });
 
-  newListing.save()
-    .then(response => {
-      console.log(`successfully created new Listing with the following checkout values ${response}`);
-    })
-    .catch(err => console.log(`there was an error creating the new listing ${err}`));
+    let newListing = new CheckoutDB({
+      priceForDate,
+      cleaningFee,
+      serviceFee,
+      'listingId': Number(i + 1),
+      'monthsInAdvance': monthsInAdvanceOptions[monthsInAdvanceIndex],
+      'daysNotice': daysNoticeOptions[daysNoticeIndex],
+    });
 
-}
+    newListing.save()
+      .then(response => {
+        console.log(`successfully created new Listing with the following checkout values ${response}`);
+      })
+      .catch(err => console.log(`there was an error creating the new listing ${err}`));
+
+  }
+} 
+
 
 const getListingCheckoutInformation = (listingId) => {
   return new Promise((resolve, reject) => {
