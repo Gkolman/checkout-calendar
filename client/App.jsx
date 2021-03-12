3f7ea3f1353ad6165c82d0162abc43387d89f6ab
galexy n <alexneeeeewin@icloud.com>
2021-03-11 20:21:39 -0800

    [feat] properly renders sample data from other teammates

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { numberOfGuests, totalReviewCount, averageReviewRatings } from '../sampleData/sampleData';
 import CheckoutCalendar from './components/CheckoutCalendar/CheckoutCalendar.js';
 
 class App extends React.Component {
--- client/components/CheckoutCalendar/CheckoutCalendar.js
+++ client/components/CheckoutCalendar/CheckoutCalendar.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import { numberOfGuests, totalReviewCount, averageReviewRatings } from '../../../sampleData/sampleData';
+
 
 class CheckoutCalendar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      allowedGuests: numberOfGuests.numberOfGuests,
+      totalReviews: totalReviewCount,
+      averageRating: averageReviewRatings.averageRating,
+    };
   }
 
   componentDidMount() {
@@ -14,9 +20,13 @@ class CheckoutCalendar extends React.Component {
   }
 
   render() {
+    const { allowedGuests, totalReviews, averageRating } = this.state;
+
     return(
       <div>
         <h1> Checkout Calendar Component Filler </h1>
+        <h2>Number of guests allowed are: {allowedGuests}</h2>
+        <h2>Average Rating: {averageRating} {totalReviews}</h2>
       </div>
     );
   }
