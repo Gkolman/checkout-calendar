945745f33f7de89f0a406dac53a2650686a4bfec
galexy n <alexneeeeewin@icloud.com>
2021-03-11 20:12:49 -0800

    [feat] made sample data and imported into app jsx

--- client/App.jsx
+++ client/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { numberOfGuests, totalReviewCount, averageReviewRatings } from '../sampleData/sampleData';
 import CheckoutCalendar from './components/CheckoutCalendar/CheckoutCalendar.js';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>testing in appjsx</h1>
         <CheckoutCalendar />
       </div>
     );
