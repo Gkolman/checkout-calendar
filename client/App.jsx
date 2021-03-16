a9fd36ed96ba407eea53b4477ebc8d15be36b49c
galexy n <alexneeeeewin@icloud.com>
2021-03-15 19:00:22 -0700

    small edit in app jsx to solve error bounds

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 import { DisplayCalendar } from './components/DisplayCalendar/DisplayCalendar';
 import CheckoutTool from './components/CheckoutTool/CheckoutTool';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checkInDate: '',
+      checkOutdate: '',
+      pricePerNight: '',
+      cleaningFee: '',
+      serviceFee: '',
+      occupancyFee: '',
+
+    };
+  }
+
+  componentDidMount() {
+
+  }
+
   render() {
     return (
       <div>
