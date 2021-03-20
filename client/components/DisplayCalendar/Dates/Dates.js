1efc1111603b181e17f6455c33bc6e7c9efba650
GalexyN <alexneeeeewin@icloud.com>
2021-03-20 15:54:16 -0700

    solved bug for displaying calendars for listings with in month bookings only

--- client/components/DisplayCalendar/Dates/Dates.js
+++ client/components/DisplayCalendar/Dates/Dates.js
@@ -15,20 +15,19 @@ class Dates extends React.Component {
       monthsInAdvance,
       currentYear,
     } = this.props;
-    let weekdayNameDivs;
+    let weekdayNameDivs = [];
     let allDates;
-
+    let combinedCalendars = [];
     let currentMonthIndex = Number(currentMonth) - 1;
-    let counter = 0;
 
-    let allowedBookingMonths = [];
-    while (counter < monthsInAdvance) {
-      weekdayNameDivs = [];
-      weekdayNames.forEach((day, index) =>
-        weekdayNameDivs.push(<div key={index}>{day}</div>)
-      );
+    weekdayNames.forEach((day, index) =>
+      weekdayNameDivs.push(<div key={index}>{day}</div>)
+    );
 
+    // returns current month as only option to book
+    if (monthsInAdvance === 0) {
       allDates = [];
+
       for (let date = 1; date <= this.props.days; date++) {
         allDates.push(
           <button
@@ -44,30 +43,85 @@ class Dates extends React.Component {
           </button>
         );
       }
-      allowedBookingMonths.push(
-        [<div className="month-name-container" key={months[currentMonthIndex]}>
-          {months[currentMonthIndex]}
-        </div>,
-        <div
-          className="weekday-names-container"
-          key={months[currentMonthIndex] + 'weekdays'}
-        >
-          {weekdayNameDivs}
-        </div>,
-        <div className="date-container" key={months[currentMonthIndex] + 'dates'}>
-        {allDates}
-        </div>]
+      combinedCalendars.push(
+        <div className="full-month-container" key={months[currentMonthIndex]}>
+          {[
+            <div
+              className="month-name-container"
+              key={months[currentMonthIndex]}
+            >
+              {months[currentMonthIndex]}
+            </div>,
+            <div
+              className="weekday-names-container"
+              key={months[currentMonthIndex] + 'weekdays'}
+            >
+              {weekdayNameDivs}
+            </div>,
+            <div
+              className="date-container"
+              key={months[currentMonthIndex] + 'dates'}
+            >
+              {allDates}
+            </div>,
+          ]}
+        </div>
       );
-      currentMonthIndex++;
-      if (currentMonthIndex > 12) {
-        currentMonthIndex = 0;
+    } else {
+      let counter = 0;
+      let allowedBookingMonths = [];
+
+      while (counter < monthsInAdvance) {
+        allDates = [];
+        for (let date = 1; date <= this.props.days; date++) {
+          allDates.push(
+            <button
+              key={date}
+              name={new Date(
+                currentYear,
+                currentMonthIndex,
+                date
+              ).toLocaleDateString()}
+              onClick={selectDate}
+            >
+              {date}
+            </button>
+          );
+        }
+
+        allowedBookingMonths.push([
+          <div className="month-name-container" key={months[currentMonthIndex]}>
+            {months[currentMonthIndex]}
+          </div>,
+          <div
+            className="weekday-names-container"
+            key={months[currentMonthIndex] + 'weekdays'}
+          >
+            {weekdayNameDivs}
+          </div>,
+          <div
+            className="date-container"
+            key={months[currentMonthIndex] + 'dates'}
+          >
+            {allDates}
+          </div>,
+        ]);
+        currentMonthIndex++;
+        if (currentMonthIndex > 12) {
+          currentMonthIndex = 0;
+        }
+        counter++;
       }
-      counter++;
+
+      allowedBookingMonths.forEach((formattedMonth, index) =>
+        combinedCalendars.push(
+          <div className="full-month-container" key={index}>
+            {formattedMonth}
+          </div>
+        )
+      );
     }
 
-    let combinedCalendars = [];
-
-    allowedBookingMonths.forEach((formattedMonth, index) => combinedCalendars.push(<div className="full-month-container" key={index}>{formattedMonth}</div>));
     // <div id="display-month">{allowedBookingMonths}</div>
     // <div id="weekday-names">{weekdayNameDivs}</div>
     // <div id="all-dates">{allDates}</div>
