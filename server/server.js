7de3d8057b1bb737f6edd9b21bf445a3c5bcea9e
gkolman <gkolman17@gmail.com>
2021-06-09 12:34:00 -0700

    running with an ec2 instance hosted db

--- server/server.js
+++ server/server.js
@@ -7,21 +7,46 @@ app.use('/:productId', express.static(path.join(__dirname, '../public')));
 var axios = require('axios');
 var newRelic = require('newrelic');
 
+const {
+  insertIntoDb,
+  getDataFromDbWithId,
+  updateIdWithData,
+  deleteDataWithId,
+  addAmountToDb,
+  deleteNumberOfRecords,
+  getSizeOfDb
+} = require('./dbms.js')
+
+app.get('/db/size', (req, res) => {
+  getSizeOfDb()
+  .then((data) => {
+    console.log('size of db -> ', data)
+    res.send(data)
+  })
+  .catch((error) => {
+    var message = `could not db size -> ${err}`
+    res.send(message)
+  })
+})
+
 app.get('/db/:id', (req, res) => {
   var id = req.params.id;
   getDataFromDbWithId(id)
   .then((data) => {
+    var data = data[0].dataValues
+    data.listingId = data.id
     res.send(data)
   })
   .catch((error) => {
-    console.log('db: error from get request -> ', error)
-    res.send(error)
+    var message = `could not get item with ${id} -> ${error}`
+    res.send(message)
   })
 })
+
 app.put('/db/:id', (req, res) => {
   var id = req.params.id;
   var data = req.body
-  updateListingInformation(id, data)
+  updateIdWithData(id, data)
   .then((data) => {
     var message  = `updated listing data for id  -> , ${id}`
     res.send(message)
@@ -34,7 +59,7 @@ app.put('/db/:id', (req, res) => {
 
 app.delete('/db/:id', (req, res) => {
   var id = req.params.id;
-  deleteListingInformation(id)
+  deleteDataWithId(id)
   .then((response) => {
     var message = `item with is ${id} has been removed from db`
     res.send(message)
@@ -44,26 +69,55 @@ app.delete('/db/:id', (req, res) => {
     res.send(message)
   })
 })
-
-app.get('/bundle.js', (req, res) => {
-  // if (Number(req.params.productId) > 100) {
-  //   res.send('invalid product id');
-  // }
-  console.log('entring bundle request')
-  res.sendFile(path.join(__dirname, '../public/bundle.js'))
-})
-
-
-app.get('/checkoutInformation/:productId', (req, res) => {
-  var id = req.params.productId
-  axios.get(`http://localhost:5000/db/${id}`)
+app.get('/testDb', (req, res) => {
+  var randomNum = (min, max) => { return Math.floor(Math.random() * (max - min)) + min}
+  var id = randomNum(1,10000000)
+  getDataFromDbWithId(1)
   .then((data) => {
-    res.send(data.data)
+    console.log('data from get request -> ', data)
+    res.send(data)
   })
   .catch((error) => {
-    var message = `could not get item with ${id} -> ${error}`
+    var message = `could not get item with ${id} -> ${err}`
     res.send(message)
   })
-});
+})
+app.post('/testDb', (req, res) => {
+  insertIntoDb()
+  .then((data) => {
+    console.log('data created successfuly in post request -> ')
+    res.send(data)
+  })
+  .catch((error) => {
+    var message = `could not add item to db -> ${err}`
+    res.send(message)
+  })
+})
+app.put('/testDb', (req, res) => {
+  var randomNum = (min, max) => { return Math.floor(Math.random() * (max - min)) + min}
+  var id = randomNum(1,10000000)
+  updateIdWithData(id,generateDataForLocation())
+  .then((data) => {
+    console.log('data updated scuccessfuly in put request')
+    res.end()
+  })
+  .catch((error) => {
+    var message = `could not update id ${id} -> ${err}`
+    res.send(message)
+  })
+})
+app.delete('/testDb', (req, res) => {
+  var randomNum = (min, max) => { return Math.floor(Math.random() * (max - min)) + min}
+  var id = randomNum(1,10000000)
+  deleteNumberOfRecords(1)
+  .then((data) => {
+    console.log('data updated scuccessfuly in put request')
+    res.end()
+  })
+  .catch((error) => {
+    console.log('put request failed -> ', error)
+    res.end()
+  })
+})
 
-app.listen(port, () => console.log('connected to server'));
+app.listen(port, () => console.log(`server is running on port ${port}`))
